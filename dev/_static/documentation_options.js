6da62c4ac4af6ae33681f23e1fa92808b2118fd6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-05-02 12:36:01 +0000

    Update docs after building Travis build 392 of AnyBody/anybody-tutorial
    The docs were built from the branch 'master' against the commit
    d08737aa9311f21885c6e9ec69964e5eac666624.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/AnyBody/anybody-tutorial/jobs/527279182.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/doc-build-environment/bin/doctr deploy --deploy-repo AnyBody/tutorials dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.2.0-dev',
+    VERSION: '7.2.1-dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
