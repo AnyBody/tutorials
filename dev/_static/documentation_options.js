b803dc32ffd7694f4af52a05c3aea14a6230aa47
melund <melund@users.noreply.github.com>
2022-04-27 13:18:15 +0000

    deploy: AnyBody/anybody-tutorial@12693ff78f399d9d864fc7fd5417a780f0f6c112

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.3.4-dev',
+    VERSION: '7.4.0-dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
