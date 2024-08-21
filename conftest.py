040cf5892a07a1efae49482b8741ab4a9fb17f4c
Morten Enemark Lund <mel@anybodytech.com>
2024-08-21 10:20:21 +0200

    Only test snippets

--- conftest.py
+++ conftest.py
@@ -14,7 +14,7 @@ def pytest_collect_file(file_path, parent):
     if file_path.suffix == ".any":
         if "Snippet" in file_path.parts[-3]:
             return AnyTestFile.from_parent(parent, path=file_path)
-        if "Downloads" in file_path.parts[-2]:
-            content = file_path.read_text()
-            if MAIN_REGEX.search(content):
-                return AnyTestFile.from_parent(parent, path=file_path)
+        # if "Downloads" in file_path.parts[-2]:
+        #     content = file_path.read_text()
+        #     if MAIN_REGEX.search(content):
+        #         return AnyTestFile.from_parent(parent, path=file_path)
